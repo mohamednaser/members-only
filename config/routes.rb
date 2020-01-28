097495fe77d0968d2c6d670a72331b1f9747eca5
Mohamed Naser <nc_m.naser@hotmail.com>
2020-01-28 02:22:03 +0200

    add sign-up feature to the project

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   get '/sign_out', to: 'sessions#delete'
 
+  get '/sign_up', to: 'users#new'
   resources :users
   resources :posts, only: %i[new create index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
